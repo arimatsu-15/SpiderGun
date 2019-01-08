37c569d783fda86509e69bec9dba1ec835da0d6d
Arimatsu Masaaki <arimatsu15@gmail.com>
2019-01-08 20:36:38 +0900

    no message

---
+++ Assets/Scripts/ReRease.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReRease : MonoBehaviour {
+
+   
+    private Rigidbody _rigidbody;
+    private CharacterJoint _characterjoint;
+
+	// Use this for initialization
+    void Start () {
+        _rigidbody = this.GetComponent<Rigidbody>();
+        _characterjoint = this.GetComponent<CharacterJoint>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(Input.GetKey("k")){
+
+            Debug.Log(_rigidbody.velocity);
+            //Vector3 start = new Vector3(_rigidbody., 0,0);
+
+            this._characterjoint.connectedBody = null;
+
+
+            //this._rigidbody.AddForce(start);
+        }
+	}
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public Transform target;
+    public float offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x = target.position.x + offset;
+        pos.y = target.position.y + offset;
+        pos.z = target.position.z + offset;
+        transform.position = pos;
+    }
+}
