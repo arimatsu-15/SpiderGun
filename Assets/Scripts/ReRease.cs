594e30972b8dcec2afdce809c91f9be6d35b940e
Arimatsu Masaaki <arimatsu15@gmail.com>
2019-01-09 00:22:45 +0900

    no message

--- Assets/Scripts/ReRease.cs
+++ Assets/Scripts/ReRease.cs
@@ -2,29 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReRease : MonoBehaviour {
+public class ReRease : MonoBehaviour
+{
 
-   
+
     private Rigidbody _rigidbody;
     private CharacterJoint _characterjoint;
-
-	// Use this for initialization
-    void Start () {
+    Vector3 leave_pos = new Vector3();
+    // Use this for initialization
+    void Start()
+    {
         _rigidbody = this.GetComponent<Rigidbody>();
         _characterjoint = this.GetComponent<CharacterJoint>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if(Input.GetKey("k")){
+    }
 
-            Debug.Log(_rigidbody.velocity);
-            //Vector3 start = new Vector3(_rigidbody., 0,0);
+    // Update is called once per frame
+    void FixedUpdate()
+    {
 
-            this._characterjoint.connectedBody = null;
+        if (Input.GetKey("l")){
 
-
-            //this._rigidbody.AddForce(start);
         }
-	}
+        if (Input.GetKey("k"))
+        {
+            Debug.Log(_rigidbody.velocity);
+            leave_pos = _rigidbody.velocity;
+            leave_parent();
+        }
+    }
+
+    public void leave_parent ()
+    {
+        //this._characterjoint.connectedBody = null;
+        Destroy(_characterjoint);
+        leave_stage();
+    }
+
+    public void leave_stage ()
+    {
+        _rigidbody.AddForce(leave_pos);
+    }
 }
