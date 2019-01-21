8b406c29b09f0aa849f81b24298690d59d55996d
Arimatsu Masaaki <arimatsu15@gmail.com>
2019-01-21 23:36:52 +0900

    no message

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,20 +6,34 @@ public class player : MonoBehaviour
 {
 
 
+    [SerializeField]
+    private GameObject ShitenKPrefab;
+    public GameObject supershitenPrefab;
+    public GameObject playerllast;
     private Rigidbody _rigidbody;
     private CharacterJoint _characterjoint;
     Vector3 leave_pos = new Vector3();
-
+    Vector3 oss;
     // Use this for initialization
     void Start()
     {
         _rigidbody = this.GetComponent<Rigidbody>();
         _characterjoint = this.GetComponent<CharacterJoint>();
+
+        oss = new Vector3(10, 10, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown("p"))
+        {
+
+            var sShitenInstance = Instantiate(supershitenPrefab,oss, Quaternion.identity) as GameObject;
+            sShitenInstance.GetComponent<CharacterJoint>().connectedBody = playerllast.GetComponent<Rigidbody>();
+
+        }
 
 
         if (Input.GetKey("k"))
@@ -28,6 +42,13 @@ public class player : MonoBehaviour
             leave_pos = _rigidbody.velocity;
             leave_parent();
         }
+
+        if (Input.GetKeyDown("l")){
+            var ShitenInstance = Instantiate(ShitenKPrefab) as GameObject;
+            ShitenInstance.GetComponent<Rigidbody>().AddForce(ShitenInstance.transform.forward * 5000f);
+        }
+
+
 
         if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
         {
@@ -38,10 +59,7 @@ public class player : MonoBehaviour
 
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
-    }
+   
 
     public void leave_parent ()
     {
